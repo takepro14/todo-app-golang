39c825662818486fae43d99a3005f824c066e160
takepro14 <takepro100pm@gmail.com>
2024-12-20 08:34:46 +0900

    feat(main): add 'Add ToDo' feature

--- main.go
+++ main.go
@@ -1,13 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+	"sync"
 )
 
+var todos = struct {
+	sync.Mutex
+	Items []string
+}{}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "TODO App Running!")
-	})
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/add", addHandler)
 	http.ListenAndServe(":8080", nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.New("index").Parse(`
+        <html>
+        <head><title>TODO App</title></head>
+        <body>
+            <h1>TODO App</h1>
+            <ul>
+                {{range .}}
+                    <li>{{.}}</li>
+                {{end}}
+            </ul>
+            <form action="/add" method="POST">
+                <input type="text" name="task" required>
+                <button type="submit">Add</button>
+            </form>
+        </body>
+        </html>
+        `))
+	todos.Lock()
+	defer todos.Unlock()
+	tmpl.Execute(w, todos.Items)
+}
+
+func addHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		task := r.FormValue("task")
+		todos.Lock()
+		todos.Items = append(todos.Items, task)
+		todos.Unlock()
+	}
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
